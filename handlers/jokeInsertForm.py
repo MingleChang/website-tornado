a1dea4a3d5b3620a7aacc7f7bcac3c2fc6670cee
Chang Junwei <mingle890805@gmail.com>
2015-09-14 17:43:18 +0800

    not

--- handlers/jokeInsertForm.py
+++ handlers/jokeInsertForm.py
@@ -4,13 +4,24 @@
 import tornado.web
 import uuid
 import tornado.httpclient
+from sqlalchemy.orm import class_mapper
 from models.models import Tag
 from handlers.base import BaseHandler
 
 class AddJokeFormHandler(BaseHandler):
 	def get(self):
-		self.render('admin/addjoke.html')
+		tagArr=self.selectAllTag()
+		self.render('admin/addjoke.html',tags=tagArr)
 
 	def post(self):
 		self.render('admin/addjoke.html')
+	
+	def selectAllTag(self):
+		tags=self.db.query(Tag)
+		tagArr=[]
+		for tag in tags:
+			columns = [c.key for c in class_mapper(tag.__class__).columns]
+			dic = dict((c, self.getAttrModel(tag, c)) for c in columns)
+			tagArr.append(dic)
+		return tagArr
 		
