030745a8d4dbd56c8eced1d6a15719b77e4dedc4
MingleChang <mingle890805@gmail.com>
2015-12-02 17:53:27 +0800

    not

--- handlers/login.py
+++ handlers/login.py
@@ -6,7 +6,27 @@ from handlers.base import BaseHandler
 
 class LoginHandler(BaseHandler):
 	def get(self):
+		if self.get_current_user():
+			self.redirect('/')
+			return
 		self.render('login.html')
 
 	def post(self):
-		self.render('login.html')
+		username=self.get_argument('username','')
+		password=self.get_argument('password','')
+		hint = self.checkUserNameAndPassword(username,password)
+		if hint=='':
+			self.set_secure_cookie('username', username)
+			self.redirect('/')
+		else:
+			self.redirect('login')
+
+	def  checkUserNameAndPassword(self,username,password):
+		cursor = self.db.cursor()
+		cursor.execute('select * from user where name=? and password=?',(username,password))
+		values = cursor.fetchall()
+		cursor.close()
+		if len(values)==0:
+			return '帐号或密码错误'
+		else:
+			return ''	
